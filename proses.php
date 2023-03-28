16169d59adf8163b0817d21f7ee6e1f605320a0b
renaldidafa <renaldidafa22@gmail.com>
2023-03-28 11:37:07 +0700

    v1.4

--- proses.php
+++ proses.php
@@ -1,4 +1,31 @@
 <?php
+<?php
+// ambil data dari form
+$angka1 = $_POST['angka1'];
+$angka2 = $_POST['angka2'];
+$operator = $_POST['operator'];
+
+// hitung hasil sesuai operator
+switch ($operator) {
+    case '+':
+        $hasil = $angka1 + $angka2;
+        break;
+    case '-':
+        $hasil = $angka1 - $angka2;
+        break;
+    case '*':
+        $hasil = $angka1 * $angka2;
+        break;
+    case '/':
+        $hasil = $angka1 / $angka2;
+        break;
+    default:
+        $hasil = "Operator tidak dikenal";
+}
+
+// tampilkan hasil
+echo "Hasil dari ".$angka1." ".$operator." ".$angka2." = ".$hasil;
+?>
 
 
  
